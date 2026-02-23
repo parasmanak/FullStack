386b002d28449829f50d6c0ac83d229b1e8e504a
Paras Manak <109068055+parasmanak@users.noreply.github.com>
2026-02-23 15:34:14 +0530

    Update User.jsx

--- Experiment-5/src/User.jsx
+++ Experiment-5/src/User.jsx
@@ -1,15 +1,16 @@
 function User() {
   return (
     <div>
-      <h1>User name is Aditya Tiwari</h1>
-      <h1>User name is Aditya Tiwari</h1>
-      <h1>User name is Aditya Tiwari</h1>
-      <h1>User name is Aditya Tiwari</h1>
-      <h1>User name is Aditya Tiwari</h1>
-      <h1>User name is Aditya Tiwari</h1>
-      <h1>User name is Aditya Tiwari</h1>
+      <h1>User name is Paras Manak</h1>
+      <h1>User name is Paras Manak</h1>
+      <h1>User name is Paras Manak</h1>
+      <h1>User name is Paras Manak</h1>
+      <h1>User name is Paras Manak</h1>
+      <h1>User name is Paras Manak</h1>
+      <h1>User name is Paras Manak</h1>
     </div>
   );
 }
 
 export default User;
+
